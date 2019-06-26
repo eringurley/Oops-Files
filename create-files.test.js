17d90dac4cac81af935716cd352c5f7956787a33
Erin Gurley <erinmgurley@gmail.com>
2019-06-26 12:10:08 -0700

    update create files test

--- create-files.test.js
+++ create-files.test.js
@@ -2,6 +2,19 @@ const fs = require('fs');
 const { getAnimal, createFiles } = require('./create-files');
 
 describe('create files', () => {
+  afterEach(done => { 
+    fs.readdir('./fixtures', (err, files) => {
+      if(files.length === 0) done();
+      let deletedSoFar = 0;
+      files.forEach(file => {
+        fs.unlink(`./fixtures/${file}`, err => {
+          if(err) return done(err);
+          deletedSoFar += 1;
+          if(deletedSoFar === files.length) done();
+        });
+      });
+    });
+  });
   it('can get a random animal species', () => {
     const animal = getAnimal();
     expect(animal).toEqual(expect.any(String));
@@ -11,8 +24,6 @@ describe('create files', () => {
     createFiles('./fixtures', 100, err => {
       expect(err).toBeFalsy();
       fs.readdir('./fixtures', { encoding: 'utf8' }, (err, files) => {
-        expect(err).toBeFalsy();
-
         expect(files).toHaveLength(100);
         done();
       });
